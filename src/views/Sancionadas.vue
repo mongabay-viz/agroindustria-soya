ce13b9bf6112c50d66507e231c73bd237e15bbb2
jdanielgoh <48138542+jdanielgoh@users.noreply.github.com>
2022-08-14 23:34:11 -0500

    Barras

--- src/views/Sancionadas.vue
+++ src/views/Sancionadas.vue
@@ -1,25 +1,42 @@
 <template>
-    <div>
-        <VizAnillos
-        :anillos_id="'anillos'"
+    <div class="vista-sancionadas">
+      <select name="estatal" id="selector-estatal" v-model="estado_seleccionado">
+        <option :value="estado" v-for="(estado,i) in lista_estatal" :key="i">
+        {{estado}}
+        </option>
+      </select>
+      <BarrasApiladas
+        :barras_id="'barras_agrupadas'"
         :datos="datos"
-        ></VizAnillos>
+        :variables="[
+          { id: 'Total', nombre_colores: 'Total', color: 'green'},
+          { id: 'No sancionatoria', nombre_colores: 'No sancionatoria', color: 'red'},
+        ]"
+        :nombre_barra="'AÑO'"
+        :nombre_color="'nombre_colores'"
+        titulo_eje_y="Eje vertical (numérico)"
+        titulo_eje_x="Eje horizontal (categórico)"
+        :alto_vis="400"
+        >
+
+      </BarrasApiladas>
+        
     </div>
 </template>
 <script>
-import { mapState } from 'vuex';
-import VizAnillos from '@/components/VizAnillos.vue'
 import * as d3 from "d3";
-
+import BarrasApiladas from "@/components/BarrasApiladas.vue"
 export default {
   name: 'Sancionadas',
   components: {
-    VizAnillos
+    BarrasApiladas
   },
   data(){
     return {
       geojson: Object,
       datos:[],
+      lista_estatal:[],
+      estado_seleccionado: "Nacional"
     }
   },
   methods:{
@@ -27,19 +44,30 @@ export default {
   },
   beforeMount(){
     d3.csv("data/sanciones.csv").then((data) => {
+      data.forEach(d=>{
+        d["No sancionatoria"] = +d["No sancionatoria"];
+        d["Total"] = +d["Total"];
+        
+      })
       this.datos_completos = data
-      this.datos = this.datos_completos.filter(d=> d.cvegeo =="19034")
+      this.lista_estatal = [...new Set(this.datos_completos.map(d=>d.estado_inegi))]
+
+      this.datos = this.datos_completos.filter(d => d.estado_inegi =="Nacional");
+      console.log(this.datos)
     }) 
     
   },
-  computed: {
-    ...mapState(["base_serie_cania"])
-  },
-  watch: {
-    base_serie_cania(nv){
-      this.data_series = nv;
-    },
+  watch:{
+    estado_seleccionado(nv){
+      this.datos = this.datos_completos.filter(d => d.estado_inegi ==nv);
+
+    }
   }
 
 }
 </script>
+<style lang="scss">
+.vista-sancionadas{
+  width: 500px
+}
+</style>
